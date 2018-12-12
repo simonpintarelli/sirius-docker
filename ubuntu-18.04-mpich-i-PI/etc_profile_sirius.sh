5ea525370c32f44791be1a17013d106ff13f0e55
Simon Pintarelli <simon.pintarelli@cscs.ch>
2018-12-12 18:11:50 +0100

    add ubuntu python2 container

---
+++ ubuntu-18.04-mpich-i-PI/etc_profile_sirius.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/opt/sirius/SIRIUS/external/lib:${LD_LIBRARY_PATH}
+export LIBSPGROOT=/opt/sirius/SIRIUS/external
---
+++ ubuntu-18.04-mpich-i-PI/install_sirius.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git clone --recursive https://github.com/simonpintarelli/SIRIUS.git -b python27_i-PI
+(
+    cd SIRIUS
+    mkdir -p external && \
+        python prerequisite.py $(realpath external) spg
+
+
+    mkdir -p build && \
+        (
+            # make sure SIRIUS finds SPG, other libs are in standard path
+            export LIBSPGROOT=/opt/sirius/SIRIUS/external
+
+            cd build
+            cmake -DCMAKE_BUILD_TYPE=Release \
+                  -DPYTHON2=On \
+                  -DCREATE_PYTHON_MODULE=On \
+                  ../
+            make install && rm -rf build && \
+                echo "Installed SIRIUS binaries to /usr/local/bin"
+            echo 'export PYTHONPATH=/usr/local/lib/python2.7/site-packages:${PYTHONPATH}' >> /etc/bash.bashrc
+        )
+)
